54e8e7b48512032c61e0dd0f89716e1950e29e3c
maxPiroddi <mpiroddi@outlook.com>
2019-10-31 08:07:06 +1100

    Account creation completed, starting on account page & details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
     def home
         @random_product = Product.order('RANDOM()').first
     end
+
+    def account
+        @account = Account.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :products
+  get "/account/:id", to: "pages#account", as: "show_account"
   root "pages#home"
 end
